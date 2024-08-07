4e54586b0e0ac29313a08ae7577de959104da5b3
Roman <romanvasenin@yandex.ru>
2024-08-07 21:57:39 +0300

    fix: small fixes

--- src/repositories/user/create.ts
+++ src/repositories/user/create.ts
@@ -5,13 +5,14 @@ import User from '#models/user.model';
 import { plainify } from '#common/types/sequelize';
 
 export async function createUser(payload: CreatePayload) {
-  const where: WhereOptions = {};
-  const hash = hashPassword(payload.password);
+  const hashPass = hashPassword(payload.password);
 
-  if (payload.name) where.name = payload.name;
-  if (payload.email) where.email = payload.email;
-  if (payload.phone) where.phone = payload.phone;
-  where.hash = hash;
+  const where: WhereOptions = {
+    name: payload.name,
+    email: payload.email,
+    phone: payload.phone,
+    hash: hashPass,
+  };
 
   const user = plainify(await User.create(where));
 
